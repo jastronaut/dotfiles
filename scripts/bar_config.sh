ce6400ad17bf5b315cc5c071328abdca7f0f6899
Jas <41971x@gmail.com>
2015-12-02 21:37:42 -0800

    added configuration for the bar w colors, fonts, and panel geometry

---
+++ scripts/bar_config.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# font
+PANEL_FONT="SFNS Display:size=13"
+
+# colors - base 16 ocean dark
+## basics
+FG="#c0c5ce"
+BG="#2b303b"
+LABEL="#65737E"
+
+## rainbow
+RED="#bf616a"
+ORANGE="#d08770"
+YELLOW="#ebcb8b"
+GREEN="#a3be8c"
+BLUE1="#96b5b4"
+BLUE2="#96b5b4"
+PURPLE="#b48ead"
+
+# geometry
+PW=2160
+PH=25
+PX=0
+PY=3
+
+# misc
+## 4 spaces
+SEP="    "
