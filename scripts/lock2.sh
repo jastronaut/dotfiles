74d51e4507366e265525aad3f67c2161ba04a995
Jas <41971x@gmail.com>
2015-12-02 21:38:52 -0800

    modified lock script to append a lock icon and the word 'locked'

--- scripts/lock2.sh
+++ scripts/lock2.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+
 scrot /tmp/screen.png
 convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
+
 [[ -f $1 ]] && convert /tmp/screen.png $1 -gravity center -composite -matte /tmp/screen.png
+
+convert /tmp/screen.png -gravity center \ -gravity center -font Reis -pointsize 90 -fill white -annotate +0+100 'locked' /tmp/screen.png - | composite -gravity center ~/scripts/lock.png  - /tmp/screen.png
+
 i3lock -i /tmp/screen.png
+
 rm /tmp/screen.png
