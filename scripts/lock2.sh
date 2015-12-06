450009e29ab10fbebb3293dc3ad088a5ff94ef74
Jas <41971x@gmail.com>
2015-12-05 20:43:33 -0800

    more verbose lock screen text

--- scripts/lock2.sh
+++ scripts/lock2.sh
@@ -5,7 +5,7 @@ convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
 
 [[ -f $1 ]] && convert /tmp/screen.png $1 -gravity center -composite -matte /tmp/screen.png
 
-convert /tmp/screen.png -gravity center \ -gravity center -font Reis -pointsize 90 -fill white -annotate +0+100 'locked' /tmp/screen.png - | composite -gravity center ~/scripts/lock.png  - /tmp/screen.png
+convert /tmp/screen.png -gravity center \ -gravity center -font Cantarell -pointsize 50 -fill white -annotate +0+100 'enter password to unlock' /tmp/screen.png - | composite -gravity center ~/scripts/lock.png  - /tmp/screen.png
 
 i3lock -i /tmp/screen.png
 
