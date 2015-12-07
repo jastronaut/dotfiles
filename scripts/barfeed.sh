cbb40b7a068e285cb0dab162717d87a0d9152d0b
Jas <41971x@gmail.com>
2015-12-06 18:59:06 -0800

    added icons. icons everywhere

--- scripts/barfeed.sh
+++ scripts/barfeed.sh
@@ -19,38 +19,40 @@ getVolume() { # get volume level from alsa thing
 	VOLUME=$(amixer get Master | grep % | sed -n 1p | awk -F '[' '{print $2}' | awk -F ']' '{print $1}')
 
 	if [ $VOLUME = '0%' ];
-		then echo -n " %{F$LABEL} MUTED %{F-} "
+		then echo -e " %{F$LABEL}$ICON_MUTED   MUTED %{F-}"
+	#elif [ $VOLUME -gt '0%' ] && [ $VOLUME -le '50%' ]
+	#	then echo -e "%{F$LABEL}VOL   $ICON_VOLUME_LO%{F-}    $VOLUME"
 	else
-		echo -n " %{F$LABEL}VOL %{F-} $VOLUME "
-fi
+		echo -e "%{F$LABEL}VOL  $ICON_VOLUME_HI%{F-}    $VOLUME"
+	fi
 }
 
 getBattery() { # battery percentage
 	BAT=$(acpi -b | grep -P -o '[0-9]+(?=%)')
-
 	if [ $BAT -le 100 ] && [ $BAT -gt 39 ]
-	   then echo "%{F$LABEL}BAT%{F-} %{F$GREEN}$BAT%%{F-}"
+	   then echo -e "%{F$LABEL}BAT%{F-} %{F$GREEN}$ICON_BAT_STATE $BAT%%{F-}"
 	elif [ $BAT -lt 40 ] && [ $BAT -gt 19 ]
-		then echo "%{F$LABEL}BAT%{F-} %{F$ORANGE}$BAT%%{F-}"
+		then echo "%{F$LABEL}BAT%{F-} %{F$ORANGE}$ICON_BAT_STATE $BAT%%{F-}"
 	elif [ $BAT -lt 20 ] && [ $BAT -gt 0 ]
-		then echo "%{F$LABEL}BAT%{F-} %{F$RED}$BAT%%{F-}"
+		then echo "%{F$LABEL}BAT%{F-} %{F$RED}$ICON_BAT_STATE $BAT%%{F-}"
 	else
-		echo "%{F$LABEL}BAT%{F-} %{F$GREEN}$BAT%%{F-}"
+		echo -e "%{F$LABEL}BAT%{F-} %{F$GREEN}$ICON_BAT_STATE $BAT%%{F-}"
 	fi
 }
 
 getMusic() { # get artist and song from playerctl 
 	ARTIST="$(playerctl metadata artist)"
 	SONG="$(playerctl metadata title)"
-	echo "%{U$PURPLE} $ARTIST %{U-} %{F$LABEL} ~ %{F-} $SONG"
+	echo -e "%{F$YELLOW}$ICON_MUSIC%{F-} $ARTIST %{F$LABEL} ~ %{F-} $SONG"
 }
 
-desk() { # get active desktop status
+desk() { # get active workspace
 	CUR=$(xprop -root _NET_CURRENT_DESKTOP | awk '{print $3}')
 	case $CUR in
-		0) echo "%{F$BLUE1}I%{F-}    II    III";;
-		1) echo "I    %{F$BLUE1}II%{F-}    III";;
-		2) echo "I    II    %{F$BLUE1}III%{F-}";;
+		0) echo -e "%{F$BLUE1}$ICON_WS1%{F-}    $ICON_WS2    $ICON_WS3    $ICON_WS4";;
+		1) echo -e "$ICON_WS1    %{F$BLUE1}$ICON_WS2%{F-}    $ICON_WS3    $ICON_WS4";;
+		2) echo -e "$ICON_WS1    $ICON_WS2    %{F$BLUE1}$ICON_WS3%{F-}    $ICON_WS4";;
+		3) echo -e "$ICON_WS1    $ICON_WS2    $ICON_WS3    %{F$BLUE1}$ICON_WS4%{F-}";;
 		*) echo "xx"
 	esac
 }
@@ -58,7 +60,7 @@ desk() { # get active desktop status
 # print out the stuff in the grossest manner possible
 while :; do
 
-    echo "%{l} $SEP$SEP$(desk) $SEP$SEP $(getMusic) $SEP$SEP  %{c}$(getClock)$SEP$(getCal)  %{r}$(getVolume) $SEP$SEP $(getBattery)$SEP$SEP"
+    echo -e "%{l} $SEP$(desk) $SEP $(getMusic) $SEP %{c}$(getClock)$SEP$ICON_DATE  $(getCal)  %{r}$(getVolume) $SEP $(getBattery)$SEP"
     sleep 1
 
-done | lemonbar -g ${PW}x${PH}+${PX}+${PY} -f "$PANEL_FONT" -F "$FG" -B "$BG" -u 2px -p -a 1
+done | lemonbar -g ${PW}x${PH}+${PX}+${PY} -f "$PANEL_FONT" -f "$PANEL_FONT2" -F "$FG" -B "$BG" -u 2px -p
