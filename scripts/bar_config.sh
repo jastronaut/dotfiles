29f8a07c6be8388768d0dfea8cf4359292afa7e3
Jas <41971x@gmail.com>
2015-12-06 18:58:25 -0800

    added bspwm stuff and fontawesome icons. im bad at making commits

--- scripts/bar_config.sh
+++ scripts/bar_config.sh
@@ -3,6 +3,7 @@
 # font
 PANEL_FONT="Cantarell:size=14"
 # PANEL_FONT="-*-candy-*-*-*--11-*-*-*-*-*-*-*"
+PANEL_FONT2="FontAwesome:size=17"
 
 # colors - base 16 ocean dark
 ## basics
@@ -20,11 +21,31 @@ BLUE2="#96b5b4"
 PURPLE="#b48ead"
 
 # geometry
-PW=2160
-PH=35
-PX=0
-PY=20
+PW=2132
+PH=37
+PX=14
+PY=22
+
+# bspwm stuff
+bspc config top_padding 57
+bspc config left_padding 0
+bspc config right_padding 0
+bspc config bottom_padding 0
 
 # misc
-## 4 spaces
-SEP="    " 
+## 8 spaces
+SEP="        " 
+
+## icons
+ICON='\uf0c2'
+ICON_BAT='\uf241'
+ICON_BAT_STATE=' '
+ICON_MUTED='\uf026'
+ICON_VOLUME_LO='\uf027'
+ICON_VOLUME_HI='\uf028'
+ICON_MUSIC='\uf001'
+ICON_DATE='\uf073'
+ICON_WS1='\uf269'
+ICON_WS2='\uf121'
+ICON_WS3='\uf120'
+ICON_WS4='\uf07c'
