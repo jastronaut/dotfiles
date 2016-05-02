01903ac58b44d7b8d91c688b912900326a7e77f9
Jas <41971x@gmail.com>
2016-05-01 17:55:12 -0700

    added 'useful' comments for future reference

--- .config/bspwm/initwifi.sh
+++ .config/bspwm/initwifi.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
+#
+# because my goddamned kernel doesn't want to load wifi shit correctly anymore
+# idk it just werked until yesterday
+# also this needs to be run with sudo
 
+# i have no idea what i'm doing; first line may not be neccessary but this is just copied from my zsh history when i somehow got wifi to work
 modprobe mwifiex
 modprobe -r mwifiex_pcie
 modprobe mwifiex_pcie
-wait 5
+# it takes some time to connect
+wait 7
+# show output of whether or not it worked
 nmcli dev
