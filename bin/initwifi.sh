a38e5368da1ea914d8c2bee743ab25c2c4eaccc6
Jas <41971x@gmail.com>
2016-05-07 10:34:51 -0700

    i do not know how to git and kept removing bspwmrc instead of initwifi bc i am a dumb

--- bin/initwifi.sh
+++ bin/initwifi.sh
@@ -5,10 +5,9 @@
 # also this needs to be run with sudo
 
 # i have no idea what i'm doing; first line may not be neccessary but this is just copied from my zsh history when i somehow got wifi to work
-modprobe mwifiex
 modprobe -r mwifiex_pcie
 modprobe mwifiex_pcie
 # it takes some time to connect
-wait 7
+sleep 5
 # show output of whether or not it worked
 nmcli dev
